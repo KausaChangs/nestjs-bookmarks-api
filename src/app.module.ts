8578479a19b4dc01b5a8f6a4a9dcb0def3de45b2
KausaChangs <changayakausa@gmail.com>
2023-03-09 10:15:37 +0200

    got signup endpoint working

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
+import * as bodyParser from 'body-parser';
 
 @Module({
   imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(bodyParser.urlencoded({ extended: true })).forRoutes('*');
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
+import bodyParser from 'body-parser';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3300);
+  app.use(bodyParser.json());
 }
 bootstrap();
